8b832929768f42da4b126eee78e4bb11a34d84cf
fatimazahraElMoutaouakkil <139513216+fatimazahraElMoutaouakkil@users.noreply.github.com>
2024-06-10 16:54:03 +0100

    Update HelloWorldController.java

--- spring-boot-hello-world-main/src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ spring-boot-hello-world-main/src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -8,6 +8,6 @@ public class HelloWorldController {
 
     @GetMapping("/")
     public String sendGreetings() {
-        return "Hello, World! MY NAME IS FATIMA ZAHRA";
+        return "Hello, World!";
     }
 }
