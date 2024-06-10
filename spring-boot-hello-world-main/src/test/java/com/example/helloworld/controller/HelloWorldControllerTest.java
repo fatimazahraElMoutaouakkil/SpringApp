1d0cfd73e4ba7f68154a65ffe3b6f2eaf0ffb9f9
fatimazahraElMoutaouakkil <139513216+fatimazahraElMoutaouakkil@users.noreply.github.com>
2024-06-10 16:53:29 +0100

    Update HelloWorldControllerTest.java

--- spring-boot-hello-world-main/src/test/java/com/example/helloworld/controller/HelloWorldControllerTest.java
+++ spring-boot-hello-world-main/src/test/java/com/example/helloworld/controller/HelloWorldControllerTest.java
@@ -22,6 +22,6 @@ public class HelloWorldControllerTest {
 
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
-                .andExpect(content().string("Hello, World! MY NAME IS FATIMA ZAHRA"));
+                .andExpect(content().string("Hello, World!"));
     }
 }
